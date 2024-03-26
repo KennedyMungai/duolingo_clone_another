5f8d52bbaa3eb25a86017f55cb97fac74d4ddeda
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 12:46:59 +0300

    Added an afterSignOutUrl to thr user button

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -30,7 +30,7 @@ const Header = () => {
 				</ClerkLoading>
 				<ClerkLoaded>
 					<SignedIn>
-						<UserButton />
+						<UserButton afterSignOutUrl='/' />
 					</SignedIn>
 					<SignedOut>
 						<SignInButton
