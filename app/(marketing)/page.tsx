0f2c1366247f4b4cc43617c125353c9a03b1d2c0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 12:14:08 +0300

    Added some layout

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,5 @@
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
-	return (
-		<main className='h-full'>
-			<div className='max-w-[200px] flex flex-col p-2 gap-2'></div>
-		</main>
-	)
+	return <div className='h-full'>Main Page</div>
 }
