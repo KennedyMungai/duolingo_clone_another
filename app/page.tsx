0c676dede57e1837912351d41e37e2a698e02616
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 11:44:48 +0300

    Added a simple button to the index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
-	return <main className='min-h-screen'></main>
+	return (
+		<main className='h-full'>
+			<Button>Click Me</Button>
+		</main>
+	)
 }
