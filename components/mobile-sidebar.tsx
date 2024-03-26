004161c58513f27c5b8c3105fc070736fa6ec527
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 15:23:53 +0300

    Updated some styles for the mobile sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import {
+	Sheet,
+	SheetClose,
+	SheetContent,
+	SheetDescription,
+	SheetFooter,
+	SheetHeader,
+	SheetOverlay,
+	SheetTitle,
+	SheetTrigger
+} from '@/components/ui/sheet'
+import SideBar from '@/components/sidebar'
+import { Menu } from 'lucide-react'
 
 type Props = {}
 
 const MobileSidebar = (props: Props) => {
-  return (
-    <div>MobileSidebar</div>
-  )
+	return (
+		<Sheet>
+			<SheetTrigger>
+				<Menu className='text-white' />
+			</SheetTrigger>
+			<SheetContent className='p-0 z-[100]' side={'left'}>
+				<SideBar />
+			</SheetContent>
+		</Sheet>
+	)
 }
 
 export default MobileSidebar
