c2b60564442481a241db8d587d2ab9465a3e89e2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 15:29:30 +0300

    Added the logo and used the default white background color for the component

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
 
 type Props = {
 	className?: string
@@ -8,11 +9,21 @@ const SideBar = ({ className }: Props) => {
 	return (
 		<div
 			className={cn(
-				'flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col',
+				'flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col',
 				className
 			)}
 		>
-			SideBar
+			<div className='pt-8 pl-4 pb-7 flex items-center gap-x-3'>
+				<Image
+					src={'/mascot.svg'}
+					alt={'logo'}
+					width={40}
+					height={40}
+				/>
+				<h1 className='text-2xl font-extrabold text-green-600 tracking-wide'>
+					Lingo
+				</h1>
+			</div>
 		</div>
 	)
 }
