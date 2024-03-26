ff1cc0ca4e1a3313e807bb36a5a52fb8f6c9df94
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 14:51:58 +0300

    Made some changes to the not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,18 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
-import React from 'react'
+import Link from 'next/link'
 
 const NotFoundPage = () => {
 	return (
-		<div className='h-full text-white bg-rose-500 flex items-center justify-center gap-5'>
-			<Image src={'mascot.svg'} width={60} height={60} alt='Mascot' />
-			<p className='font-bold text-3xl'>404 Page not found</p>
+		<div className='h-full text-white bg-orange-600 flex flex-col items-center justify-center gap-5'>
+			<div className='flex items-center gap-5 justify-center'>
+				<Image src={'mascot.svg'} width={60} height={60} alt='Mascot' />
+				<p className='font-bold text-3xl'>404 Page not found</p>
+			</div>
+
+			<Button size='lg' variant={'ghost'} asChild>
+				<Link href={'/'}>Go Back</Link>
+			</Button>
 		</div>
 	)
 }
