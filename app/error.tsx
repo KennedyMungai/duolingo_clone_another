de6155f667d31ebe666c56ea9848a4a0f5bbbeb9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 15:18:27 +0300

    Added the error message to the error screen

--- app/error.tsx
+++ app/error.tsx
@@ -20,7 +20,7 @@ const ErrorPage = ({
 		<div className='h-full text-white bg-rose-600 flex flex-col items-center justify-center gap-5'>
 			<div className='flex items-center gap-5 justify-center'>
 				<Image src={'mascot.svg'} width={60} height={60} alt='Mascot' />
-				<p className='font-bold text-3xl'>Error</p>
+				<p className='font-bold text-3xl'>Error: {error.message}</p>
 			</div>
 
 			<Button size='lg' variant={'ghost'} onClick={reset}>
