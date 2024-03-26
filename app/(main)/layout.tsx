0e6f4cd922b78f5befff8801d344acb854538705
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 15:56:43 +0300

    Removed the red background from the main part of the page

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -12,7 +12,7 @@ const MainFolderLayout = ({ children }: Props) => {
 			<MobileHeader />
 			<SideBar className='hidden lg:flex' />
 			<main className='lg:pl-[256px] h-full pt-[50px] lg:pt-0'>
-				<div className='bg-red-500 h-full'>{children}</div>
+				<div className='h-full'>{children}</div>
 			</main>
 		</>
 	)
