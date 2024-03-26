f25afdc3d5c4da8c05f6eb0d9f90a41d1e4c94ff
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 12:39:03 +0300

    Added a logo to the header componenr

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -6,12 +6,14 @@ const Header = () => {
 			<div className='lg:max-w-screen-lg mx-auto flex items-center justify-between h-full'>
 				<div className='pt-8 pl-4 pb-7 flex items-center gap-x-3'>
 					<Image
-						src={'/duolingo_icon.png'}
+						src={'/mascot.svg'}
 						alt={'logo'}
-						width={100}
-						height={100}
-						className={'rounded'}
+						width={40}
+						height={40}
 					/>
+					<h1 className='text-2xl font-extrabold text-green-600 tracking-wide'>
+						Lingo
+					</h1>
 				</div>
 			</div>
 		</header>
