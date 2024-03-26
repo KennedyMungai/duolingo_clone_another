67e123ebc9765ac1afa6c6288351dab77b629ca2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 14:31:12 +0300

    Added some styles to the home page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -21,50 +21,51 @@ export default function Home() {
 				<h1 className='text-xl lg:text-3xl font-bold text-neutral-600 text-center max-w-[480px]'>
 					Learn, Practice, and Master New Languages with Lingo.
 				</h1>
-				<div></div>
-				<ClerkLoading>
-					<Loader className='w-5 h-5 text-muted-foreground animate-spin' />
-				</ClerkLoading>
-				<ClerkLoaded>
-					<SignedOut>
-						<SignUpButton
-							mode='modal'
-							afterSignUpUrl='/learn'
-							afterSignInUrl='/learn'
-						>
+				<div className='flex flex-col items-center gap-y-3 max-w-[330px] w-full'>
+					<ClerkLoading>
+						<Loader className='w-5 h-5 text-muted-foreground animate-spin' />
+					</ClerkLoading>
+					<ClerkLoaded>
+						<SignedOut>
+							<SignUpButton
+								mode='modal'
+								afterSignUpUrl='/learn'
+								afterSignInUrl='/learn'
+							>
+								<Button
+									size='lg'
+									variant={'secondary'}
+									className='w-full'
+								>
+									Get Started
+								</Button>
+							</SignUpButton>
+							<SignInButton
+								mode='modal'
+								afterSignUpUrl='/learn'
+								afterSignInUrl='/learn'
+							>
+								<Button
+									size='lg'
+									variant={'primaryOutline'}
+									className='w-full'
+								>
+									I already have an account
+								</Button>
+							</SignInButton>
+						</SignedOut>
+						<SignedIn>
 							<Button
 								size='lg'
 								variant={'secondary'}
 								className='w-full'
+								asChild
 							>
-								Get Started
+								<Link href='/learn'>Continue Learning</Link>
 							</Button>
-						</SignUpButton>
-						<SignInButton
-							mode='modal'
-							afterSignUpUrl='/learn'
-							afterSignInUrl='/learn'
-						>
-							<Button
-								size='lg'
-								variant={'primaryOutline'}
-								className='w-full'
-							>
-								I already have an account
-							</Button>
-						</SignInButton>
-					</SignedOut>
-					<SignedIn>
-						<Button
-							size='lg'
-							variant={'secondary'}
-							className='w-full'
-							asChild
-						>
-							<Link href='/learn'>Continue Learning</Link>
-						</Button>
-					</SignedIn>
-				</ClerkLoaded>
+						</SignedIn>
+					</ClerkLoaded>
+				</div>
 			</div>
 		</div>
 	)
