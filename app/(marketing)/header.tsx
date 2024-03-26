c94fa4d902f044f71722d4c57809ee44d16cc927
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 12:35:59 +0300

    Reorganized the imports

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Image from 'next/image'
 
 const Header = () => {
 	return (
 		<header className='h-20 border-b-2 border-slate-200 px-4'>
-			Header
+			<div className='lg:max-w-screen-lg mx-auto flex items-center justify-between h-full'>
+				<div className='pt-8 pl-4 pb-7 flex items-center gap-x-3'>
+					<Image
+						src={'/duolingo_icon.png'}
+						alt={'logo'}
+						width={100}
+						height={100}
+						className={'rounded'}
+					/>
+				</div>
+			</div>
 		</header>
 	)
 }
