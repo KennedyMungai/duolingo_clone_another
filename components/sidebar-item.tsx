d5bcdba26adc95cc01b9c1c7e0597a54c34c061c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 15:43:32 +0300

    Added some markup to the sidebar item component

--- components/sidebar-item.tsx
+++ components/sidebar-item.tsx
@@ -1,9 +1,39 @@
 'use client'
 
-type Props = {}
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { Button } from './ui/button'
 
-const SideBarItem = (props: Props) => {
-	return <div>SideBarItem</div>
+type Props = {
+	label: string
+	iconSrc: string
+	href: string
+}
+
+const SideBarItem = ({ href, iconSrc, label }: Props) => {
+	const pathname = usePathname()
+
+	const active = pathname === href
+
+	return (
+		<Button
+			variant={active ? 'sidebarOutline' : 'sidebar'}
+			className='justify-start h-[52px]'
+			asChild
+		>
+			<Link href={href}>
+				<Image
+					src={iconSrc}
+					height={32}
+					width={32}
+					className='mr-5'
+					alt={label}
+				/>
+				{label}
+			</Link>
+		</Button>
+	)
 }
 
 export default SideBarItem
