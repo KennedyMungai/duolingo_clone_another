14d29e009c77e6e45e9a7f6c1fe843042f2cd077
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 16:04:06 +0300

    Added some more items to the sticky wrapper component

--- components/sticky-wrapper.tsx
+++ components/sticky-wrapper.tsx
@@ -4,8 +4,14 @@ type Props = {
 	children: ReactNode
 }
 
-const StickyWrapper = (props: Props) => {
-	return <div>StickyWrapper</div>
+const StickyWrapper = ({ children }: Props) => {
+	return (
+		<div className='hidden lg:block w-[368px] sticky self-end bottom-6'>
+			<div className='min-h-[calc(100vh)-48px] sticky top-6 flex flex-col gap-y-4'>
+				{children}
+			</div>
+		</div>
+	)
 }
 
 export default StickyWrapper
