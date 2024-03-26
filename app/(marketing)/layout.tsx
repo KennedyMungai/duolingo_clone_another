933b3aca5b19dbd7e5245ed4ed7c15efb1ea5bbf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-26 12:16:19 +0300

    Created and rendered the Header component

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react'
+import Header from './header'
 
 type Props = {
 	children: ReactNode
@@ -7,6 +8,7 @@ type Props = {
 const MarketingLayout = ({ children }: Props) => {
 	return (
 		<div className='min-h-screen flex flex-col'>
+			<Header />
 			<main className='flex-1 flex flex-col items-center justify-center'>
 				{children}
 			</main>
