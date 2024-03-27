61f5effd9544e2261cfaa1974c3f8d63a846a557
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-27 09:49:50 +0300

    Added some styles for the card

--- app/(main)/courses/card.tsx
+++ app/(main)/courses/card.tsx
@@ -1,14 +1,26 @@
+import { cn } from '@/lib/utils'
+
 type Props = {
 	id: string
 	title: string
 	imageSrc: string
-	onClick: (id: number) => void
+	onClick: (id: string) => void
 	disabled?: boolean
 	active?: boolean
 }
 
 const Card = ({ active, disabled, id, imageSrc, onClick, title }: Props) => {
-	return <div>Card</div>
+	return (
+		<div
+			onClick={() => onClick(id)}
+			className={cn(
+				'h-full border-2 rounded-xl border-b-4 hover:bg-black/5 cursor-pointer active:border-b-2 flex flex-col items-center justify-between p-3 pb-6 min-h-[217px] min-w-[200px]',
+				disabled && 'pointer-events-none opacity-50'
+			)}
+		>
+			Card
+		</div>
+	)
 }
 
 export default Card
