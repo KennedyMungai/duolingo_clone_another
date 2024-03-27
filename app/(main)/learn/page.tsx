e26037549d1efa1890ae1f37f8032764fefdcdf2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-27 15:07:14 +0300

    Showing the raw unit data

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,12 +1,13 @@
 import FeedWrapper from '@/components/feed-wrapper'
 import StickyWrapper from '@/components/sticky-wrapper'
 import UserProgress from '@/components/user-progress'
-import { getUserProgress } from '@/db/queries'
+import { getUnits, getUserProgress } from '@/db/queries'
 import { redirect } from 'next/navigation'
 import Header from './header'
 
 const LearnPage = async () => {
 	const userProgress = await getUserProgress()
+	const units = await getUnits()
 
 	if (!userProgress || !userProgress.activeCourse) {
 		redirect('/courses')
@@ -15,6 +16,11 @@ const LearnPage = async () => {
 		<div className='flex gap-[48px] px-6'>
 			<FeedWrapper>
 				<Header title={userProgress.activeCourse.title} />
+				{units.map((unit) => (
+					<div key={unit.id} className='mb-10'>
+						{JSON.stringify(unit)}
+					</div>
+				))}
 			</FeedWrapper>
 			<StickyWrapper>
 				<UserProgress
