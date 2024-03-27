349b95c5b77e4b5430794e40e7366a7a6a5eccf2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-27 15:31:37 +0300

    Added a button that links to the learn page

--- app/(main)/learn/unit-banner.tsx
+++ app/(main)/learn/unit-banner.tsx
@@ -1,3 +1,7 @@
+import { Button } from '@/components/ui/button'
+import { NotebookText } from 'lucide-react'
+import Link from 'next/link'
+
 type Props = {
 	title: string
 	description: string
@@ -10,6 +14,12 @@ const UnitBanner = ({ description, title }: Props) => {
 				<h3 className='text-2xl font-bold'>{title}</h3>
 				<p className='text-lg'>{description}</p>
 			</div>
+			<Link href='/lesson'>
+				<Button>
+					<NotebookText className='mr-2' />
+					Continue
+				</Button>
+			</Link>
 		</div>
 	)
 }
