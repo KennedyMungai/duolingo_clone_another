8975ac576999be937ba6452e9c4e2bcf7505ffbc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-27 10:38:59 +0300

    Defined the function to get user progress

--- db/queries.ts
+++ db/queries.ts
@@ -1,8 +1,26 @@
 import db from '@/db/drizzle'
+import { auth } from '@clerk/nextjs'
+import { eq } from 'drizzle-orm'
 import { cache } from 'react'
+import { userProgress } from './schema'
 
 export const getCourses = cache(async () => {
 	const data = await db.query.courses.findMany()
+
+	return data
+})
+
+const getUserProgress = cache(async () => {
+	const { userId } = await auth()
+
+	if (!userId) return null
+
+	const data = await db.query.userProgress.findFirst({
+		where: eq(userProgress.userId, userId),
+		with: {
+			activeCourse: true
+		}
+	})
 
 	return data
 })
