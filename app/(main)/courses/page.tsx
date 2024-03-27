0ff1d66e87e708a29d8af69774cb7a8e997b8a49
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-27 09:31:53 +0300

    Returned the from the database

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,11 +1,14 @@
-type Props = {}
+import { getCourses } from '@/db/queries'
 
-const CoursesPage = (props: Props) => {
+const CoursesPage = async () => {
+	const data = await getCourses()
+
 	return (
 		<div className='h-full max-w-[912px] px-3 mx-auto'>
 			<h1 className='text-2xl font-bold text-neutral-700'>
 				Language Courses
 			</h1>
+			{JSON.stringify(data)}
 		</div>
 	)
 }
