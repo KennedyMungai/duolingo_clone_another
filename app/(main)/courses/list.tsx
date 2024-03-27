7bc43d7c4aff1d4feb65f9938f650840f06475e9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-27 11:14:27 +0300

    Implemented the upsertUserProgress on the onClick function

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import { upsertUserProgress } from '@/actions/user-progress'
 import { courses, userProgress } from '@/db/schema'
+import { useRouter } from 'next/navigation'
+import { useTransition } from 'react'
 import Card from './card'
 
 type Props = {
@@ -9,6 +12,22 @@ type Props = {
 }
 
 const List = ({ activeCourseId, courses }: Props) => {
+	const router = useRouter()
+
+	const [pending, startTransition] = useTransition()
+
+	const onClick = (id: string) => {
+		if (pending) return
+
+		if (id === activeCourseId) {
+			return router.push('/learn')
+		}
+
+		startTransition(() => {
+			upsertUserProgress(id)
+		})
+	}
+
 	return (
 		<div className='pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-full,minmax(210px,1fr))] gap-4'>
 			{courses.map((course) => (
