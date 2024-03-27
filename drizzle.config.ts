7322366f84283a4708f53f49ef2e18a39cc023cc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-27 09:14:07 +0300

    Made some changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,5 +3,8 @@ import type { Config } from 'drizzle-kit'
 
 export default {
 	schema: 'db/schema.ts',
-	out: './drizzle'
+	out: './drizzle',
+	dbCredentials: {
+		connectionString: process.env.DATABASE_URL!
+	}
 } satisfies Config
