ec1ef604f9c3f5d64a36bbf7b5e17797c152951e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-27 09:45:10 +0300

    Updated the props on the Card component

--- app/(main)/courses/card.tsx
+++ app/(main)/courses/card.tsx
@@ -2,9 +2,9 @@ type Props = {
 	id: string
 	title: string
 	imageSrc: string
-	onClick: () => void
-	disabled: boolean
-	active: boolean
+	onClick: (id: number) => void
+	disabled?: boolean
+	active?: boolean
 }
 
 const Card = ({ active, disabled, id, imageSrc, onClick, title }: Props) => {
