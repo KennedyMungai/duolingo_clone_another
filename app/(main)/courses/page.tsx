1f5f4608bfc2d9879444b47c876724e85280ee89
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-27 09:28:13 +0300

    Added some markup to the courses page

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,7 +1,13 @@
 type Props = {}
 
 const CoursesPage = (props: Props) => {
-	return <div>CoursesPage</div>
+	return (
+		<div className='h-full max-w-[912px] px-3 mx-auto'>
+			<h1 className='text-2xl font-bold text-neutral-700'>
+				Language Courses
+			</h1>
+		</div>
+	)
 }
 
 export default CoursesPage
