56be87ae14120abc23a1ee8100ddc8ec57fec502
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-28 15:17:50 +0300

    Added some feedback for when a user gets an option correct

--- app/lesson/footer.tsx
+++ app/lesson/footer.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
+import { CheckCircle } from 'lucide-react'
 import { useKey, useMedia } from 'react-use'
 
 type Props = {
@@ -23,6 +24,12 @@ const Footer = ({ disabled, onCheck, status, lessonId }: Props) => {
 			)}
 		>
 			<div className='max-w-[1140px] h-full mx-auto flex items-center justify-between px-6 lg:px-10'>
+				{status === 'correct' && (
+					<div className='text-green-500 font-bold text-base lg:text-2xl flex items-center'>
+						<CheckCircle className='h-6 w-6 lg:h-10 lg:w-10 mr-4' />
+						Nicely Done!
+					</div>
+				)}
 				<Button
 					disabled={disabled}
 					className='ml-auto'
