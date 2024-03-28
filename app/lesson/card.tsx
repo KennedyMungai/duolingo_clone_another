c27e8ca7f73868f9530d304e8880997f21a4d933
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-28 12:36:08 +0300

    Made some typing updates for the props

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -3,17 +3,28 @@ import { challenges } from '@/db/schema'
 type Props = {
 	id: string
 	text: string
-	imageSrc: string
+	imageSrc: string | null
 	shortcut: string
 	selected: boolean
 	onClick: () => void
 	status: 'correct' | 'wrong' | 'none'
-	audioSrc?: string
+	audioSrc?: string | null
 	disabled?: boolean
 	type: (typeof challenges.$inferInsert)['type']
 }
 
-const Card = (props: Props) => {
+const Card = ({
+	id,
+	imageSrc,
+	onClick,
+	selected,
+	shortcut,
+	status,
+	text,
+	type,
+	audioSrc,
+	disabled
+}: Props) => {
 	return <div>Card</div>
 }
 
