5b9625e4e2e0328247be1d6d0cbd645d5ba4f5b8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-28 15:30:16 +0300

    Added some markup for messages in the correct and wring states and a button upon lesson completion

--- app/lesson/footer.tsx
+++ app/lesson/footer.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
-import { CheckCircle } from 'lucide-react'
+import { CheckCircle, XCircle, XCircleIcon } from 'lucide-react'
 import { useKey, useMedia } from 'react-use'
 
 type Props = {
@@ -29,6 +29,22 @@ const Footer = ({ disabled, onCheck, status, lessonId }: Props) => {
 						<CheckCircle className='h-6 w-6 lg:h-10 lg:w-10 mr-4' />
 						Nicely Done!
 					</div>
+				)}
+				{status === 'wrong' && (
+					<div className='text-rose-500 font-bold text-base lg:text-2xl flex items-center'>
+						<XCircle className='h-6 w-6 lg:h-10 lg:w-10 mr-4' />
+						Try Again!
+					</div>
+				)}
+				{status === 'completed' && (
+					<Button
+						size={isMobile ? 'sm' : 'lg'}
+						onClick={() =>
+							(window.location.href = `/lesson/${lessonId}`)
+						}
+					>
+						Practice Again
+					</Button>
 				)}
 				<Button
 					disabled={disabled}
