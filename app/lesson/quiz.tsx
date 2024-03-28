900cf72a28e9e0320d73e2973a273cee90ea1375
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-28 09:56:40 +0300

    Updated the data type if the initialLessonChallenges

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,8 +1,15 @@
 'use client'
 
+import { challenges } from '@/db/schema'
+import { challengeOptions } from '@/db/schema'
+
 type Props = {
 	initialLessonId: string
-	initialLessonChallenges: any[]
+	initialLessonChallenges: typeof challenges.$inferSelect &
+		{
+			completed: boolean
+			challengeOptions: (typeof challengeOptions.$inferSelect)[]
+		}[]
 	initialHearts: number
 	initialPercentage: number
 	userSubscription: any
