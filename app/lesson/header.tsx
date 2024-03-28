a9eceecc93cdd8022a29d9dcd1ba6eda188abb05
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-28 10:55:57 +0300

    Deleted the todo

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -15,7 +15,7 @@ const Header = ({ hasActiveSubscription, hearts, percentage }: Props) => {
 	return (
 		<header className='lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full'>
 			<X
-				onClick={open} //TODO: Add onclick exit
+				onClick={open}
 				className='text-slate-500 hover:opacity-75 transition cursor-pointer'
 			/>
 			<Progress value={percentage} />
