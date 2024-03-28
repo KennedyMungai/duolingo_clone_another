ba290c505ff55a8a3f733194580c83bc16d68ec5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-28 09:50:15 +0300

    Defined the initialPercentage const

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -1,4 +1,5 @@
 import { getLesson, getUserProgress } from '@/db/queries'
+import { lessons } from '@/db/schema'
 import { redirect } from 'next/navigation'
 
 const LessonPage = async () => {
@@ -6,6 +7,12 @@ const LessonPage = async () => {
 	const userProgress = await getUserProgress()
 
 	if (!lesson || !userProgress) redirect('/learn')
+
+	const initialPercentage =
+		Math.floor(
+			lesson.challenges.filter((challenge) => challenge.completed)
+				.length / lesson.challenges.length
+		) * 100
 
 	return <div>LessonPage</div>
 }
