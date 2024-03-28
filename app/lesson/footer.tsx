9a52b35414623ea3d2914cbed176d36703eb4036
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-28 15:13:18 +0300

    Added some logic to make the styling and the text of the button on the footer dynamic

--- app/lesson/footer.tsx
+++ app/lesson/footer.tsx
@@ -1,11 +1,41 @@
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { useKey, useMedia } from 'react-use'
+
 type Props = {
-	disabled: boolean
-	status: 'correct' | 'wrong' | 'none'
+	disabled?: boolean
+	status: 'correct' | 'wrong' | 'none' | 'completed'
 	onCheck: () => void
+	lessonId?: boolean
 }
 
-const Footer = ({ disabled, onCheck, status }: Props) => {
-	return <div>Footer</div>
+const Footer = ({ disabled, onCheck, status, lessonId }: Props) => {
+	const isMobile = useMedia('(max-width: 1024px)')
+
+	return (
+		<footer
+			className={cn(
+				'lg:h-[140px] h-[100px] border-t-2',
+				status === 'correct' && 'border-transparent bg-green-100',
+				status === 'wrong' && 'border-transparent bg-rose-100'
+			)}
+		>
+			<div className='max-w-[1140px] h-full mx-auto flex items-center justify-between px-6 lg:px-10'>
+				<Button
+					disabled={disabled}
+					className='ml-auto'
+					onClick={onCheck}
+					size={isMobile ? 'sm' : 'lg'}
+					variant={status === 'wrong' ? 'danger' : 'secondary'}
+				>
+					{status === 'none' && 'Check'}
+					{status === 'correct' && 'Next'}
+					{status === 'wrong' && 'Retry'}
+					{status === 'completed' && 'Continue'}
+				</Button>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
