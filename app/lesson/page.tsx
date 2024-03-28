6245de2d79a227e8382d8c1c4c541e7be74dbcb5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-28 09:48:03 +0300

    Getting the lesson and userProgress data and added a null checker for which the nu;ll fields redirect to the learn page

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -1,6 +1,12 @@
-type Props = {}
+import { getLesson, getUserProgress } from '@/db/queries'
+import { redirect } from 'next/navigation'
 
-const LessonPage = (props: Props) => {
+const LessonPage = async () => {
+	const lesson = await getLesson()
+	const userProgress = await getUserProgress()
+
+	if (!lesson || !userProgress) redirect('/learn')
+
 	return <div>LessonPage</div>
 }
 
