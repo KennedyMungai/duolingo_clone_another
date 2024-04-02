6c5db47b953432a3b806b7bb79fc06c4e3e4d01d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-02 12:51:15 +0300

    Addedsome markup to the Shop page

--- app/(main)/shop/page.tsx
+++ app/(main)/shop/page.tsx
@@ -1,5 +1,44 @@
-const ShopPage = () => {
-	return <div>ShopPage</div>
+import FeedWrapper from '@/components/feed-wrapper'
+import StickyWrapper from '@/components/sticky-wrapper'
+import UserProgress from '@/components/user-progress'
+import { getUserProgress } from '@/db/queries'
+import Image from 'next/image'
+import { redirect } from 'next/navigation'
+
+const ShopPage = async () => {
+	const userProgress = await getUserProgress()
+
+	if (!userProgress || !userProgress.activeCourse) redirect('/courses')
+
+	return (
+		<div className='flex gap-[48px] px-6'>
+			<FeedWrapper>
+				<div className='w-full flex flex-col items-center'>
+					<Image
+						src={'/shop.svg'}
+						height={90}
+						width={90}
+						alt='Shop'
+					/>
+					<h1 className='text-center font-bold text-neutral-800 text-2xl my-6'>
+						Shop
+					</h1>
+					<p className='text-muted-foreground text-center text-lg mb-6'>
+						Spend your points on cool stuff
+					</p>
+					<Items />
+				</div>
+			</FeedWrapper>
+			<StickyWrapper>
+				<UserProgress
+					activeCourse={userProgress.activeCourse}
+					hearts={userProgress.hearts}
+					points={userProgress.points}
+					hasActiveSubscription={false}
+				/>
+			</StickyWrapper>
+		</div>
+	)
 }
 
 export default ShopPage
