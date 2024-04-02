ecc4e4aef708a0308669390ccf7b2e88c833e265
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-02 14:34:51 +0300

    Defined the server function to refill the hearts

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -8,6 +8,9 @@ import { and, eq } from 'drizzle-orm'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
+// TODO: Move alongside item component constant into common file
+const POINTS_TO_REFILL = 10
+
 export const upsertUserProgress = async (courseId: string) => {
 	const { userId } = await auth()
 	const user = await currentUser()
@@ -102,4 +105,29 @@ export const reduceHearts = async (challengeId: string) => {
 	revalidatePath('/quests')
 	revalidatePath('/leaderboard')
 	revalidatePath(`/lesson/${lessonId}`)
+}
+
+export const refillHearts = async () => {
+	const currentUserProgress = await getUserProgress()
+
+	if (!currentUserProgress) throw new Error('User progress not found')
+
+	if (currentUserProgress.hearts === 5)
+		throw new Error('Hearts are already full')
+
+	if (currentUserProgress.points < POINTS_TO_REFILL)
+		throw new Error('Not Enough Points')
+
+	await db
+		.update(userProgress)
+		.set({
+			hearts: 5,
+			points: currentUserProgress.points - POINTS_TO_REFILL
+		})
+		.where(eq(userProgress.userId, currentUserProgress.userId))
+
+	revalidatePath('/shop')
+	revalidatePath('/learn')
+	revalidatePath('/quests')
+	revalidatePath('/leaderboard')
 }
