8a3913c94b6cf5901f6cea3a63a1ee1a44242858
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-02 15:30:16 +0300

    Added some more stripe logic

--- app/(main)/shop/items.tsx
+++ app/(main)/shop/items.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { refillHearts } from '@/actions/user-progress'
+import { createStripeUrl } from '@/actions/user-subscription'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import { useTransition } from 'react'
@@ -27,8 +28,20 @@ const Items = ({ hasActiveSubscription, hearts, points }: Props) => {
 		})
 	}
 
+	const onUpgrade = () => {
+		startTransition(() => {
+			createStripeUrl()
+				.then((response) => {
+					if (response.data) {
+						window.location.href = response.data
+					}
+				})
+				.catch(() => toast.error('Something went wrong'))
+		})
+	}
+
 	return (
-		<ul className='w-full'>
+		<div className='w-full'>
 			<div className='flex items-center w-full p-4 gap-x-4 border-t-2'>
 				<Image src='/heart.svg' alt='Heart' width={60} height={60} />
 				<div className='flex-1'>
@@ -57,7 +70,26 @@ const Items = ({ hasActiveSubscription, hearts, points }: Props) => {
 					)}
 				</Button>
 			</div>
-		</ul>
+			<div className='flex items-center w-full p-4 pt-8 gap-x-4 border-t-2'>
+				<Image
+					src='/unlimited.svg'
+					alt='unlimited'
+					height={60}
+					width={60}
+				/>
+				<div className='flex-1'>
+					<p className='text-neutral-700 text-base lg:text-xl font-bold'>
+						Unlimited Hearts
+					</p>
+				</div>
+				<Button
+					disabled={pending || hasActiveSubscription}
+					onClick={onUpgrade}
+				>
+					{hasActiveSubscription ? 'active' : 'upgrade'}
+				</Button>
+			</div>
+		</div>
 	)
 }
 
