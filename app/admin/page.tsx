3e5ad6cc31b30b1c747ab53a2d282d85eaa86b3b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-03 12:27:26 +0300

    Redirecting to the root page if a user is not admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,14 @@
+import { getIsAdmin } from '@/lib/admin'
 import dynamic from 'next/dynamic'
+import { redirect } from 'next/navigation'
 
 const AdminApp = dynamic(() => import('./app'), { ssr: false })
 
-const AdminPage = () => {
+const AdminPage = async () => {
+	const isAdmin = await getIsAdmin()
+
+	if (!isAdmin) redirect('/')
+
 	return <AdminApp />
 }
 
