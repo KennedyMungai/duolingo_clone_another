5c502f39771b9f880d14637e752d08ca51702693
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-03 10:32:05 +0300

    The basic markup for the quests page

--- app/(main)/quests/page.tsx
+++ app/(main)/quests/page.tsx
@@ -1,20 +1,13 @@
 import FeedWrapper from '@/components/feed-wrapper'
 import StickyWrapper from '@/components/sticky-wrapper'
-import { Avatar, AvatarImage } from '@/components/ui/avatar'
-import { Separator } from '@/components/ui/separator'
 import UserProgress from '@/components/user-progress'
-import {
-	getTopTenUsers,
-	getUserProgress,
-	getUserSubscription
-} from '@/db/queries'
+import { getUserProgress, getUserSubscription } from '@/db/queries'
 import Image from 'next/image'
 import { redirect } from 'next/navigation'
 
 const QuestsPage = async () => {
 	const userProgress = await getUserProgress()
 	const userSubscription = await getUserSubscription()
-	const leaderboard = await getTopTenUsers()
 
 	if (!userProgress || !userProgress.activeCourse) redirect('/courses')
 
@@ -23,41 +16,18 @@ const QuestsPage = async () => {
 			<FeedWrapper>
 				<div className='w-full flex flex-col items-center'>
 					<Image
-						src={'/leaderboard.svg'}
+						src={'/quests.svg'}
 						height={90}
 						width={90}
-						alt='leaderboard'
+						alt='Quests'
 					/>
 					<h1 className='text-center font-bold text-neutral-800 text-2xl my-6'>
-						Leaderboard
+						Quests
 					</h1>
 					<p className='text-muted-foreground text-center text-lg mb-6'>
-						See where you stand among other learners in the
-						community
+						Complete quests by earning points
 					</p>
-					<Separator className='mb-4 h-0.5 rounded-full' />
-					{leaderboard.map((userProgress, index) => (
-						<div
-							key={index}
-							className='flex items-center w-full p-2 px-4 rounded-xl hover:bg-gray-200/50'
-						>
-							<p className='font-bold text-lime-700 mr-4'>
-								{index + 1}
-							</p>
-							<Avatar className='border bg-green-500 h-12 w-12 ml-3 mr-6'>
-								<AvatarImage
-									src={userProgress.userImageSrc}
-									className='object-cover'
-								/>
-							</Avatar>
-							<p className='font-bold text-neutral-800 flex-1'>
-								{userProgress.userName}
-							</p>
-							<p className='text-muted-foreground'>
-								{userProgress.points} XP
-							</p>
-						</div>
-					))}
+					{/* TODO: Add quests */}
 				</div>
 			</FeedWrapper>
 			<StickyWrapper>
