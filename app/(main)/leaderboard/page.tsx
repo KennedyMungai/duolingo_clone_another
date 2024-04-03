2dfd69ae1eb27744e762f99c9fd1114d72620629
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-03 10:21:59 +0300

    Added teh top users to the leaderboard page

--- app/(main)/leaderboard/page.tsx
+++ app/(main)/leaderboard/page.tsx
@@ -1,5 +1,7 @@
 import FeedWrapper from '@/components/feed-wrapper'
 import StickyWrapper from '@/components/sticky-wrapper'
+import { Avatar, AvatarImage } from '@/components/ui/avatar'
+import { Separator } from '@/components/ui/separator'
 import UserProgress from '@/components/user-progress'
 import {
 	getTopTenUsers,
@@ -33,8 +35,28 @@ const LeaderBoardPage = async () => {
 						See where you stand among other learners in the
 						community
 					</p>
+					<Separator className='mb-4 h-0.5 rounded-full' />
 					{leaderboard.map((userProgress, index) => (
-						<div key={index}>{userProgress.userName}</div>
+						<div
+							key={index}
+							className='flex items-center w-full p-2 px-4 rounded-xl hover:bg-gray-200/50'
+						>
+							<p className='font-bold text-lime-700 mr-4'>
+								{index + 1}
+							</p>
+							<Avatar className='border bg-green-500 h-12 w-12 ml-3 mr-6'>
+								<AvatarImage
+									src={userProgress.userImageSrc}
+									className='object-cover'
+								/>
+							</Avatar>
+							<p className='font-bold text-neutral-800 flex-1'>
+								{userProgress.userName}
+							</p>
+							<p className='text-muted-foreground'>
+								{userProgress.points} XP
+							</p>
+						</div>
 					))}
 				</div>
 			</FeedWrapper>
