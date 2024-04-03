cb859e8453658a54097b1c223322cab481ab0b12
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-03 10:44:17 +0300

    Added some static quests to the quests page

--- app/(main)/quests/page.tsx
+++ app/(main)/quests/page.tsx
@@ -1,9 +1,33 @@
 import FeedWrapper from '@/components/feed-wrapper'
 import StickyWrapper from '@/components/sticky-wrapper'
+import { Progress } from '@/components/ui/progress'
 import UserProgress from '@/components/user-progress'
 import { getUserProgress, getUserSubscription } from '@/db/queries'
 import Image from 'next/image'
 import { redirect } from 'next/navigation'
+
+const quests = [
+	{
+		title: 'Earn 20 XP',
+		value: 20
+	},
+	{
+		title: 'Earn 50 XP',
+		value: 50
+	},
+	{
+		title: 'Earn 100 XP',
+		value: 100
+	},
+	{
+		title: 'Earn 500 XP',
+		value: 500
+	},
+	{
+		title: 'Earn 1000 XP',
+		value: 1000
+	}
+]
 
 const QuestsPage = async () => {
 	const userProgress = await getUserProgress()
@@ -27,7 +51,35 @@ const QuestsPage = async () => {
 					<p className='text-muted-foreground text-center text-lg mb-6'>
 						Complete quests by earning points.
 					</p>
-					{/* TODO: Add quests */}
+					<ul className='w-full'>
+						{quests.map((quest, index) => {
+							const progress =
+								(userProgress.points / quest.value) * 100
+
+							return (
+								<div
+									key={index}
+									className='flex items-center w-full p-4 gap-x-4 border-t-2'
+								>
+									<Image
+										src='/points.svg'
+										alt='Points'
+										width={60}
+										height={60}
+									/>
+									<div className='flex flex-col gap-y-2 w-full'>
+										<p className='text-neutral-700 text-xl font-bold'>
+											{quest.title}
+										</p>
+										<Progress
+											value={progress}
+											className='h-3'
+										/>
+									</div>
+								</div>
+							)
+						})}
+					</ul>
 				</div>
 			</FeedWrapper>
 			<StickyWrapper>
